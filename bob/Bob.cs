25d8790adbd5ff175c220aa11395565309f7cc84
Abhilash K <abhilash.face@gmail.com>
2016-10-03 02:55:06 +0530

    Done Bob

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -19,31 +19,58 @@ namespace Exercism_CSharp.csharp.bob
             {
                 outputString = "Sure.";
             }
+            else if (saidNothing(inputString))
+            {
+                outputString = "Fine. Be that way!";
+            }
             return outputString;
         }
 
         public static bool isShout(string inputString)
         {
+            bool hasCharacter = false;
             for (int i = 0; i < inputString.Length; i++)
             {
                 if (Char.IsLetter(inputString[i]))
                 {
+                    hasCharacter = true;
                     if (Char.IsLower(inputString[i]))
                     {
                         return false;
                     }
                 }
             }
-            return true;
+            return hasCharacter;
         }
 
         public static bool isQuestion(string inputString)
         {
+            inputString = inputString.Trim();
+            if (inputString.Length == 0)
+            {
+                return false;
+            }
             if ((inputString[(inputString.Length - 1)]).Equals('?'))
             {
                 return true;
             }
             return false;
+        }
+
+        public static bool saidNothing(string inputString)
+        {
+            if(inputString.Length == 0)
+            {
+                return true;
+            }
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if(inputString[i] != ' ')
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
