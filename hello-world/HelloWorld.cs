36ec011ab06d78d179181f84ec69dd14c637c9c2
Abhilash K <abhilash.face@gmail.com>
2016-10-02 22:03:14 +0530

    Hello World

---
+++ hello-world/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism_CSharp.csharp.hello_world
+{
+    public class HelloWorld
+    {
+        public static string Hello(string inputString)
+        {
+            string outputString = "Hello, World!";
+
+            if(inputString != null)
+            {
+                outputString = "Hello, " + inputString + "!";
+            }
+            return outputString;
+        }
+    }
+}
