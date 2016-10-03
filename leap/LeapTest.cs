51ed7b22163aa43dd002e80e650c711b068545b5
Abhilash K <abhilash.face@gmail.com>
2016-10-03 21:43:30 +0530

    Done Leap

--- leap/LeapTest.cs
+++ leap/LeapTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Exercism_CSharp.csharp.leap;
 
 [TestFixture]
 public class LeapTest
@@ -9,21 +10,21 @@ public class LeapTest
         Assert.That(Year.IsLeap(1996), Is.True);
     }
 
-    [Ignore("Remove to run test")]
+    //[Ignore("Remove to run test")]
     [Test]
     public void Invalid_leap_year()
     {
         Assert.That(Year.IsLeap(1997), Is.False);
     }
 
-    [Ignore("Remove to run test")]
+    //[Ignore("Remove to run test")]
     [Test]
     public void Turn_of_the_20th_century_is_not_a_leap_year()
     {
         Assert.That(Year.IsLeap(1900), Is.False);
     }
 
-    [Ignore("Remove to run test")]
+    //[Ignore("Remove to run test")]
     [Test]
     public void Turn_of_the_25th_century_is_a_leap_year()
     {
---
+++ leap/Year.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism_CSharp.csharp.leap
+{
+    public class Year
+    {
+        public static bool IsLeap(int inputYear)
+        {
+            if(inputYear % 4 == 0)
+            {
+                if(inputYear % 100 == 0)
+                {
+                    if(inputYear % 400 == 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
